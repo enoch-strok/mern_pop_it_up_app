44896ba92bf641314873636b256aa192613b0583
enoch-strok <enochstrok@outlook.com>
2021-01-05 21:00:11 -0800

    Added reading code to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
 import MyNewComponentActual from './components/MyNewComponent';
+import LightSwitch from './components/LightSwitchComponent';
 
 function App() {
   return (
     <div className="App">
+      <LightSwitch/>
+      lightswitch
+      
       <MyNewComponentActual firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"}/>
       <MyNewComponentActual firstName={"John"} lastName={"Smith"} age={"88"} hairColor={"Brown"}/>
       <MyNewComponentActual firstName={"Millard"} lastName={"Fillmore"} age={"50"} hairColor={"Brown"}/>
       <MyNewComponentActual firstName={"Maria"} lastName={"Smith"} age={"62"} hairColor={"Brown"}/>
+      <MyNewComponentActual header={"Header Prop"}>
+        <p>This is a child1</p>
+        <p>This is a child2</p>
+        <p>This is a child3</p>
+        <p>This is a child4</p>
+      </MyNewComponentActual>
+      <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
+      <button onChange={ ()=> alert("On Change!") }>onChange</button>
+      <button onSubmit={ ()=> alert("On Submit!") }>onSubmit</button>
+      <button onFocus={ ()=> alert("On Focus!") }>onFocus</button>
+      <button onBlur={ ()=> alert("Lost Focus - Blur!") }>onBlur</button>
     </div>
       
   );
--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+// import { render } from 'react-dom';
 
 class MyNewComponentActual extends Component {
     render(){
         const { firstName, lastName, age, hairColor } = this.props;
         return(
             <div>
-                {this.props.someText}
-                <h1>My name is { lastName }, { firstName}  </h1>
-                <p>Age:  { age }</p>
-                <p>Hair Color: { hairColor }</p>
+                <section>
+                    {this.props.someText}
+                    <h1>My name is { lastName }, { firstName}  </h1>
+                    <p>Age:  { age }</p>
+                    <p>Hair Color: { hairColor }</p>
+                </section>
+                <section>
+                    <h1>
+                        {this.props.header}
+                    </h1>
+                    {this.props.children}
+                </section>
             </div>
         );
     }
 }
 
+
 export default MyNewComponentActual;
+
